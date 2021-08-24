992e7939c4a342d28cc10f1f7359e5d6e3b489f5
othienoJoe <bryo094@gmail.com>
2021-08-24 15:58:38 +0300

    created goal-detail component after modifying the goal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Goal } from './goal';
 })
 export class AppComponent {
   goals:Goal[] = [
-    {id:1, name:'Watch Coming Home'},
-    {id:2, name:'Buy underpants'},
-    {id:3, name:'Attend last class'},
-    {id:4, name:'Check the previous project'},
-    {id:5, name:'Finish content for today'},
-    {id:6, name:'Read a book'},
+    // {id:1, name:'Watch Coming Home'},
+    // {id:2, name:'Buy underpants'},
+    // {id:3, name:'Attend last class'},
+    // {id:4, name:'Check the previous project'},
+    // {id:5, name:'Finish content for today'},
+    // {id:6, name:'Read a book'},
   ];
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Goal  } from '../goal';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+
+  goals:Goal[] = [
+    {id:1, name:'Watch Coming Home', description:''},
+    {id:2, name:'Buy underpants', description:''},
+    {id:3, name:'Attend last class', description:''},
+    {id:4, name:'Check the previous project', description:''},
+    {id:5, name:'Finish content for today', description:''},
+    {id:6, name:'Read a book', description:''},
+  ];
 
   constructor() { }
 
