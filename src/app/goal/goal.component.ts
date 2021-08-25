a35df1661bc2c3435a5bb35102bcdaa10522eeec
othienoJoe <bryo094@gmail.com>
2021-08-25 07:38:25 +0300

    modified goal component and strikethrough directive then created date-count pipe

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,12 +8,12 @@ import { Goal  } from '../goal';
 export class GoalComponent implements OnInit {
 
   goals:Goal[] = [
-    new Goal(1, 'Watch Coming Home', 'Done'),
-    new Goal(2, 'Buy underpants', 'Done'),
-    new Goal(3, 'Attend last class', 'Done'),
-    new Goal(4, 'Check the previous project', 'Done'),
-    new Goal(5, 'Finish content for today', 'Done'),
-    new Goal(6, 'Read a book', 'Done'),
+    new Goal(1, 'Watch Coming Home', 'Done', new Date(2021,7,25)),
+    new Goal(2, 'Buy underpants', 'Done', new Date(2021,7,25)),
+    new Goal(3, 'Attend last class', 'Done', new Date(2021,7,25)),
+    new Goal(4, 'Check the previous project', 'Done', new Date(2021,7,25)),
+    new Goal(5, 'Finish content for today', 'Done', new Date(2021,7,25)),
+    new Goal(6, 'Read a book', 'Done', new Date(2021,7,25)),
   ];
 
   toggleDetails(index: any) {
